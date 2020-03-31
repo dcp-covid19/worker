8039b5c3e9ac73aa5ad6cd5403681ca237a8c112
KCErb <kc@kcerb.com>
2020-03-31 17:45:51 -0400

    keep screen awak

--- src/components/StartWorkerButton.js
+++ src/components/StartWorkerButton.js
@@ -1,23 +1,22 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import demoKeystore from '../demoKeystore';
+import NoSleep from 'nosleep.js'
+const noSleep = new NoSleep();
 
 const StartWorkerButton = ({ updateStatus }) => {
   const [running, setRunning] = useState(false);
 
   useEffect(() => {
     const onStart = () => {
-      console.log('start worker button onstart listenre')
       setRunning(true);
     }
     const onStop = () => {
-      console.log('start worker button on stop listenre')
       setRunning(false);
     }
     window.dcp.compute.supervisor.on('start', onStart);
     window.dcp.compute.supervisor.on('stop', onStop);
 
     return () => {
-      console.log('deregistering listeners from start worker button')
       window.dcp.compute.supervisor.off('start', onStart);
       window.dcp.compute.supervisor.off('stop', onStop);
     }
@@ -31,17 +30,19 @@ const StartWorkerButton = ({ updateStatus }) => {
     let countSpan = document.getElementById('fighters-count');
     
     if (nextRunning) {
-      let keystore = await new window.dcp.wallet.Keystore(demoKeystore)
-      await keystore.unlock(null, 24 * 60 * 60)
+      let keystore = await new window.dcp.wallet.Keystore(demoKeystore);
+      await keystore.unlock(null, 24 * 60 * 60);
       window.dcp.wallet.add(keystore)
       window.dcp.compute.mine(4);
+      noSleep.enable();
       if (countSpan) countSpan.innerText = parseFloat(countSpan.innerText) + 1
     } else {
       window.dcp.compute.stopMining();
+      noSleep.disable();
       updateStatus('ready')
       if (countSpan) countSpan.innerText = parseFloat(countSpan.innerText) - 1
     }
-  }, [running, setRunning]);
+  }, [running, updateStatus]);
 
   return (
     <button className={`btn btn-${running? 'outline-' : ''}success`} onClick={onClick} style={{ width: '100px' }}>
