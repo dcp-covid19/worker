a4ff4d4bb26abd24fc4b119ba63f37c3197b0fbf
KCErb <kc@kcerb.com>
2020-04-10 16:25:00 -0400

    guard statsSummary from bad json

--- src/components/StatsSummary.js
+++ src/components/StatsSummary.js
@@ -15,18 +15,25 @@ const StatsSummary = () => {
     delay: 10 * 1000
   }
   
-  let stats = useAPIPolling(options)
-
+  let rawStats = useAPIPolling(options);
+  
+  // sanitize data from untrusted json source
+  let myStats = {}; 
+  if (rawStats.worker) myStats.ready = true
+  myStats.taskCount = rawStats.worker && rawStats.worker.distSlices && rawStats.worker.distSlices.total;
+  myStats.fighterCount = rawStats.uniqueIds;
+  if (isNaN(Number(myStats.taskCount))) myStats.taskCount = '...';
+  if (isNaN(Number(myStats.fighterCount))) myStats.fighterCount = '...';
 
   return <div id="stats-summary">
     <p style={{fontWeight: "bold"}}>Status</p>
     <p style={{margin: 0, fontSize: "0.9em"}}>(Updates every 5 minutes)</p>
     <div className="console">
-    {!stats.worker && "Loading stats from network ..."}
-    {stats.worker &&
+    {!myStats.ready && "Loading stats from network ..."}
+    {myStats.ready &&
       <>
-        <p>Tasks on Network: {stats.worker.distSlices.total}</p>
-        <p>COVID-19 Fighters: <span id="fighters-count">{stats.uniqueIds}</span></p>
+        <p>Tasks on Network: {myStats.taskCount}</p>
+        <p>COVID-19 Fighters: <span id="fighters-count">{myStats.fighterCount}</span></p>
       </>
     }
     </div>
